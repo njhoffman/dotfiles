4df2144b09e78b6b77d66da254534c3a92d5264b
Nicholas Hoffman <njhoffman1982@gmail.com>
2021-05-12 03:49:44 -0500

    updates

--- .zsh/common/plugins.zsh
+++ .zsh/common/plugins.zsh
@@ -1,56 +1,61 @@
+# install zplug if not exists
 [[ ! -d ~/.zplug ]] &&
-  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+  curl -sL --proto-redir -all,https \
+  https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 CWD="${0:A:h}"
 
 source ~/.zplug/init.zsh
 
-# Make sure to use double quotes
-zplug "kazhala/dotbare"
-zplug "plugins/git",   from:oh-my-zsh
 source "${CWD}/plugins/alias-tips.zsh"
-source "${CWD}/plugins/auto-ls.zsh"
-source "${CWD}/plugins/asdf.zsh"
-source "${CWD}/plugins/enhancd.zsh"
 source "${CWD}/plugins/anyframe.zsh"
-source "${CWD}/plugins/zsh-vim-mode.zsh"
+source "${CWD}/plugins/asdf.zsh"
+source "${CWD}/plugins/auto-ls.zsh"
+source "${CWD}/plugins/enhancd.zsh"
 source "${CWD}/plugins/zsh-auto-notify.zsh"
+source "${CWD}/plugins/zsh-autocomplete.zsh"
 source "${CWD}/plugins/zsh-autoenv.zsh"
+source "${CWD}/plugins/zsh-better-npm-completion.zsh"
+source "${CWD}/plugins/zsh-histdb.zsh"
 source "${CWD}/plugins/zsh-syntax-highlighting.zsh"
+source "${CWD}/plugins/zsh-vim-mode.zsh"
+zplug "kazhala/dotbare"
 zplug "b4b4r07/emoji-cli", on:"stedolan/jq"
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
+
+# Oh my zsh plugins
+zplug "plugins/aws",               from:oh-my-zsh
+zplug "plugins/colorize",          from:oh-my-zsh
+zplug "plugins/colored-man-pages", from:oh-my-zsh
+zplug "plugins/command-not-found", from:oh-my-zsh
+zplug "plugins/cp",                from:oh-my-zsh
+zplug "plugins/docker",            from:oh-my-zsh
+zplug "plugins/extract",           from:oh-my-zsh
+zplug "plugins/git",               from:oh-my-zsh
+zplug "plugins/helm",              from:oh-my-zsh
+zplug "plugins/kubectl",           from:oh-my-zsh
+zplug "plugins/node",              from:oh-my-zsh
+zplug "plugins/npm",               from:oh-my-zsh
+zplug "plugins/nvm",               from:oh-my-zsh
+zplug "plugins/pass",              from:oh-my-zsh
+zplug "plugins/sudo",              from:oh-my-zsh
+zplug "plugins/systemadmin",       from:oh-my-zsh
+zplug "plugins/terraform",         from:oh-my-zsh
+zplug "plugins/taskwarrior",       from:oh-my-zsh
+zplug "plugins/vi-mode",           from:oh-my-zsh
+zplug "plugins/web-search",        from:oh-my-zsh
+zplug "plugins/zsh_reload",        from:oh-my-zsh
 
 # zplug "b4b4r07/httpstat", as:command, use:'(*).sh', rename-to:'$1'
 # zplug "stedolan/jq", from:gh-r,  as:command, rename-to:jq
-# aws
-# colorize
-# colored-man-pages
-# command-not-found
-# compleat
-# cp
-# docker
-# # dotenv (want to specifically load with autoenv)
-# extract
-# git
-# helm
-# # jira
-# kubectl
-# node
-# npm
-# nvm
-# pass
-# sudo
-# systemadmin
-# taskwarrior
-# terraform
-# vi-mode
-# web-search
-# zsh_reload
 
-# Load theme file
+# Theme file
 # zplug 'dracula/zsh', as:theme
+# zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
 zplug "romkatv/powerlevel10k", as:theme, depth:1
 # time (repeat 1000 zsh -dfis <<< 'source ~/powerlevel10k/powerlevel10k.zsh-theme')
+# zplug "eendroroy/alien-minimal"
+# zplug mafredri/zsh-async, from:github
+# zplug sindresorhus/pure, use:pure.zsh, from:github, as:theme
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
